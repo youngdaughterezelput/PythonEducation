b031fab35c6c19b951fd58ea7650a08e8c2a3db4
Sofiia Fedorova <milkyloy@gmail.com>
2025-08-30 19:36:16 +0300

    1935: Обновлено readme; обновлены тесты для тестовых стендов

---
+++ Edu_Playwright/1_test/test_1.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Playwright, sync_playwright
+
+def test_loc(page):
+    page.goto('https://zimaev.github.io/text_input/')
+    page.get_by_label("Email address").fill("qa@example.com")
+    page.get_by_title("username").fill("Anton")
+    page.get_by_placeholder('password').fill("secret")
+    page.get_by_role('checkbox').click()
+
+def test_or(page):
+    selector = page.locator("input").or_(page.locator("text"))
+    selector.fill("Hello Stepik")
+
+def test_locator_and(page):
+    page.goto("https://zimaev.github.io/locatorand/")
+    selector = page.get_by_role("button", name="Sing up").and_(page.get_by_title("Sing up today"))
+    selector.click()
